aeac3d702fdea85d4a0cc09d45d22e2d86ec53e7
Jackson <jackson.hardaker@gmail.com>
2018-08-06 10:27:26 -0400

    Add store and import styles

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+// Styles
+import './styles/scss/main.scss';
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+});
