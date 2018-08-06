01f2225b38dbb2d937ab20d291e1828ce2878d1d
Jackson <jackson.hardaker@gmail.com>
2018-08-06 10:28:04 -0400

    Add routing for home and about

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
+
+import PageHome from '@/components/page/PageHome';
+import PageAbout from '@/components/page/PageAbout';
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: PageHome
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: PageAbout
     }
-  ]
-})
+  ],
+  mode: 'history'
+});
