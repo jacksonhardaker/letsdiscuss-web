330b41d836c2b9c25a0829a0a1ae4fe724eaa610
Jackson <jackson.hardaker@gmail.com>
2018-08-07 13:01:53 -0400

    Add mutation constants

---
+++ src/store/mutation.const.js
@@ -0,0 +1,18 @@
+export const ARTICLE = {
+  request: 'ARTICLE_REQUEST',
+  success: 'ARTICLE_SUCCESS',
+  error: 'ARTICLE_ERROR'
+};
+
+export const AUTH = {
+  facebook: {
+    request: 'FACEBOOK_AUTH_REQUEST',
+    success: 'FACEBOOK_AUTH_SUCCESS',
+    error: 'FACEBOOK_AUTH_ERROR'
+  },
+  google: {
+    request: 'GOOGLE_AUTH_REQUEST',
+    success: 'GOOGLE_AUTH_SUCCESS',
+    error: 'GOOGLE_AUTH_ERROR'
+  }
+};
