087d138db3328c0e333b14b9ba665eedeaea864b
Jackson <jackson.hardaker@gmail.com>
2018-08-07 13:03:13 -0400

    Add article component page

---
+++ src/components/page/PageArticle.vue
@@ -0,0 +1,33 @@
+<template>
+    <div :class="$options.name">
+    <h1>About</h1>
+
+    <p>{{ article }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: `PageArticle`,
+  computed: {
+    ...mapGetters({
+      status: 'getArticleStatus',
+      article: 'getCurrentArticle'
+    })
+  },
+  methods: {
+    ...mapActions([
+      'getCurrentArticle'
+    ])
+  },
+  created: function() {
+    console.log(
+      `[Attempting to load article... ${decodeURIComponent(this.$route.path)}]`
+    );
+
+    this.getCurrentArticle(this.$route.params);
+  }
+};
+</script>
