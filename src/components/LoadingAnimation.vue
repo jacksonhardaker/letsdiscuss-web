780bb345f6be9826dcfe28f9025ec2774000c8b2
Jackson <jackson.hardaker@gmail.com>
2018-08-07 21:29:42 -0400

    Add getters

--- src/components/LoadingAnimation.vue
+++ src/components/LoadingAnimation.vue
@@ -1,5 +1,5 @@
 <template>
-    <aside class="loading" v-bind:class=" { 'loading--animate': animate }">
+    <aside class="loading" v-bind:class=" { 'loading--animate': animate, 'loading--show': show }">
         <em class="loading-symbol">#</em>
         <em class="loading-symbol">*</em>
         <em class="loading-symbol">@</em>
@@ -17,10 +17,35 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
+function delayAnimation() {
+  const delay = this.showLoadingAnimation ? 0 : 400;
+  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(this.showLoadingAnimation), delay);
+  });
+}
+
 export default {
   name: 'LoadingAnimation',
-  props: {
-      animate: Boolean
+  computed: {
+    ...mapGetters({
+      showLoadingAnimation: 'showLoadingAnimation'
+    }),
+    show() {
+      return this.showLoadingAnimation;
+    }
+  },
+  asyncComputed: {
+    animate: {
+      async get() {
+        return await delayAnimation.call(this);
+      },
+      watch() {
+        this.showLoadingAnimation;
+      }
+    }
   }
 };
 </script>
@@ -34,108 +59,111 @@ $scale-large--loading: scale(8);
 $dimensions--loading: 100px;
 
 .loading {
-    width: $dimensions--loading;
-    height: $dimensions--loading;
-    position: fixed;
-    top: calc(50vh - #{$dimensions--loading});
-    left: calc(50% - #{$dimensions--loading});
-    border: 60px solid transparent;
-    display: none;
-    background-color: $white;
-    z-index: 9999;
+  width: $dimensions--loading;
+  height: $dimensions--loading;
+  position: fixed;
+  top: calc(50vh - #{$dimensions--loading});
+  left: calc(50% - #{$dimensions--loading});
+  border: 60px solid transparent;
+  background-color: $white;
+  z-index: 9999;
+  opacity: 0;
+  transition: opacity 0.4s linear;
+  // pointer-events: none;
 }
 
-.loading--animate {
-    display: block;
+.loading--show {
+  opacity: 1;
 }
 
 .loading--animate > .loading-symbol {
-    animation: explode 2s ease-in-out infinite;
+  animation: explode 2s ease-in-out infinite;
 }
 
 .loading-symbol {
-    position: absolute;
-    opacity: 0;
+  position: absolute;
+  opacity: 0;
+  font-style: normal;
 
-    &:nth-child(1) {
-        left: 5px;
-        top: 5px;
-        animation-delay: (0 * $animation-increment) + s;
-    }
-    &:nth-child(2) {
-        left: 20px;
-        top: 25px;
-        animation-delay: (1 * $animation-increment) + s;
-    }
-    &:nth-child(3) {
-        right: 8px;
-        top: 14px;
-        animation-delay: (2 * $animation-increment) + s;
-    }
-    &:nth-child(4) {
-        left: 45px;
-        top: 30px;
-        animation-delay: (3 * $animation-increment) + s;
-    }
-    &:nth-child(5) {
-        right: 12px;
-        bottom: 11px;
-        animation-delay: (4 * $animation-increment) + s;
-    }
-    &:nth-child(6) {
-        left: 7px;
-        bottom: 2px;
-        animation-delay: (5 * $animation-increment) + s;
-    }
-    &:nth-child(7) {
-        left: 23px;
-        bottom: 18px;
-        animation-delay: (6 * $animation-increment) + s;
-    }
-    &:nth-child(8) {
-        right: 30px;
-        top: 50px;
-        animation-delay: (7 * $animation-increment) + s;
-    }
-    &:nth-child(9) {
-        left: 18px;
-        bottom: 2px;
-        animation-delay: (8 * $animation-increment) + s;
-    }
-    &:nth-child(10) {
-        left: 47px;
-        bottom: 2px;
-        animation-delay: (9 * $animation-increment) + s;
-    }
-    &:nth-child(11) {
-        right: 0px;
-        bottom: 0px;
-        animation-delay: (10 * $animation-increment) + s;
-    }
-    &:nth-child(12) {
-        left: 45px;
-        top: 11px;
-        animation-delay: (11 * $animation-increment) + s;
-    }
-    &:nth-child(13) {
-        left:50px;
-        top: 50px;
-        animation-delay: (12 * $animation-increment) + s;
-    }
+  &:nth-child(1) {
+    left: 5px;
+    top: 5px;
+    animation-delay: (0 * $animation-increment) + s;
+  }
+  &:nth-child(2) {
+    left: 20px;
+    top: 25px;
+    animation-delay: (1 * $animation-increment) + s;
+  }
+  &:nth-child(3) {
+    right: 8px;
+    top: 14px;
+    animation-delay: (2 * $animation-increment) + s;
+  }
+  &:nth-child(4) {
+    left: 45px;
+    top: 30px;
+    animation-delay: (3 * $animation-increment) + s;
+  }
+  &:nth-child(5) {
+    right: 12px;
+    bottom: 11px;
+    animation-delay: (4 * $animation-increment) + s;
+  }
+  &:nth-child(6) {
+    left: 7px;
+    bottom: 2px;
+    animation-delay: (5 * $animation-increment) + s;
+  }
+  &:nth-child(7) {
+    left: 23px;
+    bottom: 18px;
+    animation-delay: (6 * $animation-increment) + s;
+  }
+  &:nth-child(8) {
+    right: 30px;
+    top: 50px;
+    animation-delay: (7 * $animation-increment) + s;
+  }
+  &:nth-child(9) {
+    left: 18px;
+    bottom: 2px;
+    animation-delay: (8 * $animation-increment) + s;
+  }
+  &:nth-child(10) {
+    left: 47px;
+    bottom: 2px;
+    animation-delay: (9 * $animation-increment) + s;
+  }
+  &:nth-child(11) {
+    right: 0px;
+    bottom: 0px;
+    animation-delay: (10 * $animation-increment) + s;
+  }
+  &:nth-child(12) {
+    left: 45px;
+    top: 11px;
+    animation-delay: (11 * $animation-increment) + s;
+  }
+  &:nth-child(13) {
+    left: 50px;
+    top: 50px;
+    animation-delay: (12 * $animation-increment) + s;
+  }
 }
 
 @keyframes explode {
-    0% {
-        opacity: 0;
-        transform: $scale-small--loading;
-    }
-    50% {
-        opacity: 1;
-    }
-    100% {
-        opacity: 0;
-        transform: $scale-large--loading;
-    }
+  0% {
+    opacity: 0;
+    transform: $scale-small--loading;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    transform: $scale-large--loading;
+  }
 }
 </style>
 
