2fbadf0658acbd5a533fd0df0931485e317e21f8
Jackson <jackson.hardaker@gmail.com>
2018-08-07 20:15:26 -0400

    Add base url component

---
+++ src/components/BaseURL.vue
@@ -0,0 +1,17 @@
+<template>
+    <span>
+        {{domain}}
+    </span>
+</template>
+
+<script>
+export default {
+  name: 'BaseURL',
+  props: ['src'],
+  computed: {
+    domain() {
+      return this.src ? new URL(this.src).hostname : '';
+    }
+  }
+};
+</script>
