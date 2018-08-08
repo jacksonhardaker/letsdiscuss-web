f5fc8be85a6961956a130ebf3e229fac966f1c0c
Jackson <jackson.hardaker@gmail.com>
2018-08-07 21:30:06 -0400

    Use async computed

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import AsyncComputed from 'vue-async-computed';
 import App from './App';
 import router from './router';
 import store from './store';
 import axios from 'axios';
 import Cookies from 'js-cookie';
+
+Vue.use(AsyncComputed);
 
 // Styles
 import './styles/scss/main.scss';
