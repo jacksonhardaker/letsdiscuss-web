08a41a5f4231cf208cf15149c34fb4e3a8930e54
Jackson <jackson.hardaker@gmail.com>
2018-08-07 21:29:23 -0400

    Add global loading animation

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
   <div id="app">
     <router-view/>
+    <LoadingAnimation/>
   </div>
 </template>
 
 <script>
+import LoadingAnimation from './components/LoadingAnimation';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    LoadingAnimation
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
