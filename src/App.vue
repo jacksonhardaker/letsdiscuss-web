a8d01560967497acab72a715beba2c2a6bfb07ca
Jackson <jackson.hardaker@gmail.com>
2018-08-08 22:05:42 -0400

    Add blur effect

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    <router-view/>
-    <GlobalLoadingAnimation/>
+    <router-view class="app" v-bind:class=" { 'blurred': !isAuthenticated }"/>
+    <global-loading-animation/>
+    <login-overlay/>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 import GlobalLoadingAnimation from './components/GlobalLoadingAnimation';
+import LoginOverlay from './components/LoginOverlay';
 
 export default {
   name: 'App',
+  computed: {
+      ...mapGetters(
+          ['isAuthenticated']
+      )
+  },
   components: {
-    GlobalLoadingAnimation
+    GlobalLoadingAnimation,
+    LoginOverlay
   }
 }
 </script>
+
+<style lang="scss">
+.app {
+  filter: blur(0);
+  transition: filter 0.4s ease-in;
+}
+
+.app.blurred {
+  filter: blur(5px);
+}
+</style>
