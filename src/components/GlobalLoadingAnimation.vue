ae313ea383f44293390034c7c7dd7c9c8223edec
Jackson <jackson.hardaker@gmail.com>
2018-08-08 22:05:53 -0400

    Change to lowercase element tag

--- src/components/GlobalLoadingAnimation.vue
+++ src/components/GlobalLoadingAnimation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="global-loading">
-    <LoadingAnimation :show="show" :animate="animate"/>
+    <loading-animation :show="show" :animate="animate"/>
   </div>
 </template>
 
