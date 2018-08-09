be327bc095aaa535c07d861e7796a42a84d5417e
Jackson <jackson.hardaker@gmail.com>
2018-08-08 22:06:03 -0400

    Add login overlay

---
+++ src/components/LoginOverlay.vue
@@ -0,0 +1,45 @@
+<template>
+    <section v-if="!isAuthenticated" class="loginOverlay">
+        <facebook-login-button/>
+        <google-login-button/>
+    </section>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import FacebookLoginButton from './buttons/FacebookLoginButton';
+import GoogleLoginButton from './buttons/GoogleLoginButton';
+
+export default {
+  name: 'LoginOverlay',
+  computed: {
+      ...mapGetters(
+          ['isAuthenticated']
+      )
+  },
+  components: {
+    FacebookLoginButton,
+    GoogleLoginButton
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/scss/colors.scss';
+@import '../styles/scss/z-index.scss';
+
+.loginOverlay {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: $zIndex-9--overlay;
+  background-color: $black-t7;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
