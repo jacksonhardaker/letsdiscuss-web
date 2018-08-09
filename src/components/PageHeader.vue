c8670e8ffc3d69ba0d855b351a43c02919fc73dd
Jackson <jackson.hardaker@gmail.com>
2018-08-09 11:49:46 -0400

    Add page header

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="pageHeader shadow-1">
+      <person-avatar :picture="currentPerson.picture" :name="currentPerson.name"/></header>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import PersonAvatar from './icons/PersonAvatar';
+
+export default {
+  name: 'PageHeader',
+  computed: {
+    ...mapGetters(['currentPerson'])
+  },
+  components: {
+    PersonAvatar
+  }
+};
+</script>
+
+<style lang="scss">
+.pageHeader {
+  height: 60px;
+  width: 100%;
+}
+</style>
