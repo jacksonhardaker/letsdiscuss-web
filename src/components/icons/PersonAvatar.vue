1b32a2f3192021ba18dadbd82fbd8c39125c4fc9
Jackson <jackson.hardaker@gmail.com>
2018-08-09 11:49:25 -0400

    Add avatar

---
+++ src/components/icons/PersonAvatar.vue
@@ -0,0 +1,30 @@
+<template>
+    <figure class="personAvatar">
+        <img class="personAvatar--picture" :src="picture" :title="name" :alt="name"/>
+    </figure>
+</template>
+
+<script>
+export default {
+  name: 'PersonAvatar',
+  props: ['picture', 'name']
+};
+</script>
+
+
+<style lang="scss" scoped>
+.personAvatar {
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  overflow: hidden;
+  margin: 0;
+}
+
+.personAvatar--picture {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+}
+</style>
+
