986a37c53d908285831704b74cb4ccdd66685af1
Jackson <jackson.hardaker@gmail.com>
2018-08-09 20:20:12 -0400

    Add alias avatar

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,6 +1,9 @@
 <template>
     <header class="pageHeader shadow-1">
-      <person-avatar :picture="currentPerson.picture" :name="currentPerson.name"/></header>
+      <person-avatar :picture="personPicture" :name="personName"/>
+      <span class="font-h4">=</span>
+      <person-avatar :picture="aliasPicture" :name="aliasName"/>
+    </header>
 </template>
 
 <script>
@@ -11,7 +14,19 @@ import PersonAvatar from './icons/PersonAvatar';
 export default {
   name: 'PageHeader',
   computed: {
-    ...mapGetters(['currentPerson'])
+    ...mapGetters(['currentPerson', 'currentAlias']),
+    personPicture() {
+      return this.currentPerson ? this.currentPerson.picture : '';
+    },
+    personName() {
+      return this.currentPerson ? this.currentPerson.name : '';
+    },
+    aliasPicture() {
+      return this.currentAlias ? this.currentAlias.pictureUrl : '';
+    },
+    aliasName() {
+      return this.currentAlias ? this.currentAlias.name : '';
+    }
   },
   components: {
     PersonAvatar
@@ -19,9 +34,12 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .pageHeader {
   height: 60px;
   width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 </style>
