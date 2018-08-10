09f8d7977eb9855665db13c62c7cf511d715c887
Jackson <jackson.hardaker@gmail.com>
2018-08-09 20:19:44 -0400

    Add missing state

--- src/components/icons/PersonAvatar.vue
+++ src/components/icons/PersonAvatar.vue
@@ -1,18 +1,28 @@
 <template>
     <figure class="personAvatar">
-        <img class="personAvatar--picture" :src="picture" :title="name" :alt="name"/>
+        <img v-if="!missing" class="personAvatar--picture" :src="picture" :title="name" :alt="name"/>
+        <div v-if="missing" class="personAvatar--missing flex--col-cc">
+            <span class="font-h5">?</span>
+        </div>
     </figure>
 </template>
 
 <script>
 export default {
   name: 'PersonAvatar',
-  props: ['picture', 'name']
+  props: ['picture', 'name'],
+  computed: {
+    missing() {
+      return !(!!this.picture && !!this.name);
+    }
+  }
 };
 </script>
 
 
 <style lang="scss" scoped>
+@import '../../styles/scss/colors.scss';
+
 .personAvatar {
   width: 44px;
   height: 44px;
@@ -22,9 +32,16 @@ export default {
 }
 
 .personAvatar--picture {
-    object-fit: cover;
-    width: 100%;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+}
+
+.personAvatar--missing {
+    background-color: $black-t3;
     height: 100%;
+    width: 100%;
     border-radius: 50%;
 }
 </style>
