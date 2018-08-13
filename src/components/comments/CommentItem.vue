faf3d2b3327aee176359b070d45b8f08d0442dec
Jackson <jackson.hardaker@gmail.com>
2018-08-13 16:42:12 -0400

    Handle null

--- src/components/comments/CommentItem.vue
+++ src/components/comments/CommentItem.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="commentItem">
         <div class="commentItem--content flex--row-ss">
-            <person-avatar class="commentItem--avatar" :picture="comment.alias.pictureUrl" :name="comment.alias.name"/>
+            <person-avatar class="commentItem--avatar" :picture="picture" :name="name"/>
             <p class="commentItem--body">{{comment.body}}</p>
         </div>
         <reply-list class="commentItem--replies" :replies="comment.replies"/>
@@ -15,6 +15,14 @@ import PersonAvatar from '../icons/PersonAvatar.vue';
 export default {
     name: 'CommentItem',
     props: ['comment'],
+    computed: {
+        picture() {
+            return this.comment && this.comment.alias ? this.comment.alias.pictureUrl : '';
+        },
+        name() {
+            return this.comment && this.comment.alias ? this.comment.alias.name : '';
+        }
+    },
     components: {
         ReplyList,
         PersonAvatar
@@ -23,4 +31,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../styles/scss/colors.scss';
+
+.commentItem {
+    background-color: $blue-lighter;
+    margin: 10px 0;
+}
 </style>
