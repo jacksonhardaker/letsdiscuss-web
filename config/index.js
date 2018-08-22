11e072bcb2d2c2d25689ce2461b95dfb5545021f
Jackson <jackson.hardaker@gmail.com>
2018-08-22 09:27:40 -0400

    Update public path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../letsdiscuss-preview'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/letsdiscuss-web/',
+    assetsPublicPath: '/letsdiscuss-preview/',
 
     /**
      * Source Maps
