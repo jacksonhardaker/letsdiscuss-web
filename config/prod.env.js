1bcf5477dc3de804f4da94e77cbe794bf2293760
Jackson <jackson.hardaker@gmail.com>
2018-08-22 09:46:14 -0400

    Update base url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  WEB_BASE_URL: '"/letsdiscuss-web"',
+  WEB_BASE_URL: '"/letsdiscuss-preview"',
   API_URL : '"https://letsdiscuss-211513.appspot.com"'
 }
